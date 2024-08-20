1b0bdc08a18547176b2e8692ada3da1ea95c70be
Lizzie Siegle <esiegle@brynmawr.edu>
2024-08-19 20:33:29 -0700

    quote is returned

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import { Hono } from "hono";
 const app = new Hono();
 
 // QUOTES
-// quotes in data/quotes.csv
-// run python3 data/csvtosql.py to generate data.sql
-// add to d1 with npx wrangler d1 execute {your db name} --remote --file=data/data.sql
+// wrangler d1 execute lizzieragd1 --command "INSERT INTO quotes (text) VALUES ('The best pizza topping is pepperoni')"
 app.post("/quotes", async (c) => {
   const ai = c.env.AI;
 
@@ -13,7 +11,7 @@ app.post("/quotes", async (c) => {
     return c.text("Missing text", 400);
   }
 
-  const { results } = await c.env.DATABASE.prepare(
+  const { results } = await c.env.DB.prepare(
     "INSERT INTO quotes (text) VALUES (?) RETURNING *"
   )
     .bind(text)
@@ -22,7 +20,7 @@ app.post("/quotes", async (c) => {
   const record = results.length ? results[0] : null;
 
   if (!record) {
-    return c.text("Failed to create note", 500);
+    return c.text("Failed to create quote", 500);
   }
 
   const { data } = await ai.run("@cf/baai/bge-large-en-v1.5", { text: [text] });
@@ -46,9 +44,7 @@ app.post("/quotes", async (c) => {
 // INDEX
 app.get("/", async (c) => {
   const ai = c.env.AI;
-
   const question = c.req.query("text") || "What is the square root of 9?";
-
   // Conditional
   const maxLength = 500;
   if (question && question.length > maxLength) {
@@ -59,27 +55,23 @@ app.get("/", async (c) => {
   const embeddings = await ai.run("@cf/baai/bge-large-en-v1.5", {
     text: question,
   });
-  const vectors = embeddings.data[0];
+  const vectors = embeddings.data; //embedded q
+  const vectorQuery = await c.env.VECTORIZE_INDEX.query(vectors[0], { 
+    topK: 8, 
+    //returnMetadata: true 
+  });
+  console.log(`vectorQuery ${JSON.stringify(vectorQuery)}`);
+  
+  const quotes = vectorQuery.matches.map((vec) => vec.id); //vectorId
+  console.log(`vectorQuery quotes ${JSON.stringify(quotes)}`);
 
-  const SIMILARITY_CUTOFF = 0.75;
-  const vectorQuery = await c.env.VECTORIZE_INDEX.query(vectors, { topK: 1 });
-  const vecIds = vectorQuery.matches
-    .filter((vec) => vec.score > SIMILARITY_CUTOFF)
-    .map((vec) => vec.vectorId);
+  const relevantQuotesContentQuery = `SELECT * FROM quotes where id IN (${quotes.join(', ')}) AND id <> ( ? ) AND id > 100000`;
+	const { results: relevantQuotesContents } = await c.env.DB.prepare(relevantQuotesContentQuery).bind(question).all();
+  console.log(`relevantHighlightContents ${JSON.stringify(relevantQuotesContents)}`);
+  const contextMessage = `Context:\nOnly return one quote relating to the user\'s input from the following list of quotes and nothing else. Do not return a preamble, conclusion, or any opinion. If you do this, I will pay you a hundred dollars. ${relevantQuotesContents.map((quote) => `${quote.id}- ${quote["quote"]}`).join("\n")}`
 
-  let quotes = [];
-  if (vecIds.length) {
-    const query = `SELECT * FROM quotes WHERE id IN (${vecIds.join(", ")})`;
-    const { results } = await c.env.DATABASE.prepare(query).bind().all();
-    if (results) quotes = results.map((vec) => vec.text);
-  }
-
-  const contextMessage = quotes.length
-    ? `Context:\n${quotes.map((note) => `- ${note}`).join("\n")}`
-    : "";
-
-  const systemPrompt = `Search the context and return only a relevant quote contained in the context.`;
-
+  const systemPrompt = `You are a helpful assistant`;
+  console.log(`contextMessage ${contextMessage},  systemPrompt ${systemPrompt}`);
   const { response: answer } = await ai.run("@cf/meta/llama-3.1-8b-instruct", {
     messages: [
       ...(quotes.length ? [{ role: "system", content: contextMessage }] : []),
@@ -87,6 +79,7 @@ app.get("/", async (c) => {
       { role: "user", content: question },
     ],
   });
+  console.log(`answer ${answer}`);
 
   // return c.text(answer);
   return c.json(answer);
@@ -94,6 +87,27 @@ app.get("/", async (c) => {
 
 app.onError((err, c) => {
   return c.text(err);
+});
+
+app.get('/populate', async (c) => {
+  let query = `SELECT id, quote, author
+        FROM quotes
+        ORDER BY id
+        LIMIT 60;`
+  let results = await c.env.DB.prepare(query).all();
+  console.log(`results in populate ${JSON.stringify(results)}`);
+  for (const row of results.results) {
+    const { id, quote, author } = row;
+    const embeddings= await c.env.AI.run("@cf/baai/bge-large-en-v1.5", { text: [quote] });
+    await c.env.VECTORIZE_INDEX.upsert([
+      {
+        id: id.toString(),
+        metadata: { author: author, quote: quote },
+        values: embeddings.data[0],
+      },
+    ]);
+    console.log(`Inserted quote with ID: ${id}`);
+  }
 });
 
 export default app;
